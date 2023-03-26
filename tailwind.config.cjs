c30815f192f0fa75aa730fc927591771e444ef15
carloszbent <bentsoft365@gmail.com>
2023-03-25 22:51:59 -0400

    added automatic theme-change basde on user settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	plugins: [require("daisyui")],
+	darkTheme: "dark",
 	daisyui: {
 		themes: [
 		  {
@@ -15,7 +17,18 @@ module.exports = {
 				"warning": "#dc2626",
 				"error": "#EF4D78",
 			},
-		  },
+			dark: {
+				"primary": "#cacaca",
+				"secondary": "#f59e0b",
+				"accent": "#b4d1e1",
+				"neutral": "#292524",
+				"base-100": "#f3f4f6",
+				"info": "#819AD9",
+				"success": "#73E8A6",
+				"warning": "#dc2626",
+				"error": "#EF4D78",
+			}
+		},
 		],
 	  },
 	theme: {
