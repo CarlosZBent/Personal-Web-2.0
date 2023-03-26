e48d12b5f4d64bbb2673e68d685a9fb07230c4e5
carloszbent <bentsoft365@gmail.com>
2023-03-25 23:34:34 -0400

    set up dark mode colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require("daisyui")],
 	darkTheme: "dark",
 	daisyui: {
 		themes: [
@@ -18,11 +17,11 @@ module.exports = {
 				"error": "#EF4D78",
 			},
 			dark: {
-				"primary": "#cacaca",
+				"primary": "#4b5563",      
 				"secondary": "#f59e0b",
-				"accent": "#b4d1e1",
+				"accent": "#991b1b",
 				"neutral": "#292524",
-				"base-100": "#f3f4f6",
+				"base-100": "#111827",
 				"info": "#819AD9",
 				"success": "#73E8A6",
 				"warning": "#dc2626",
