0395771a967b24e923168f17d45b03d47ab0c427
carloszbent <bentsoft365@gmail.com>
2023-05-04 10:54:08 -0400

    added supabase_conn module

---
+++ src/supabase_conn.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseURL = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
+const supabaseConn = createClient(supabaseURL, supabaseKey);
+
+export async function insertIntoSupabase(table: String, jsonData) {
+    const { data, error } = await supabaseConn.from(String(table)).insert(
+        [jsonData]
+      )
+}
