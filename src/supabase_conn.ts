ff4754b2a2da645c70e330ca6ebb704f3b290710
carloszbent <bentsoft365@gmail.com>
2023-05-05 16:27:37 -0400

    added typescript to enforce types on supabase table data

--- src/supabase_conn.ts
+++ src/supabase_conn.ts
@@ -4,7 +4,13 @@ const supabaseURL = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
 const supabaseConn = createClient(supabaseURL, supabaseKey);
 
-export async function insertIntoSupabase(table: String, jsonData) {
+export interface SupabaseAnalyticsTableRow {
+  date: string,
+  event_type: string,
+  additional_data?: string 
+}
+
+export async function insertIntoSupabase(table: String, jsonData: SupabaseAnalyticsTableRow) {
     const { error } = await supabaseConn.from(String(table)).insert(
         [jsonData]
       )
